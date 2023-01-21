9123c9329b34a7674a71bac51700fc02d96169e7
unknown <collinbrown226@gmail.com>
2023-01-21 12:56:22 -0600

    Create helper function for knight movement

--- chess_piece.py
+++ chess_piece.py
@@ -204,17 +204,44 @@ class Rook(Piece):
         super().__init__(team, ROOK, location)
 
     def get_moves(self, row: int, col: int, board: list) -> list:
-        possible_moves = []
-        possible_moves += self.orthogonal_up(row, col, board)
-        possible_moves += self.orthogonal_left(row, col, board)
-        possible_moves += self.orthogonal_down(row, col, board)
-        possible_moves += self.orthogonal_right(row, col, board)
-        return possible_moves
+        moves = []
+        moves += self.orthogonal_up(row, col, board)
+        moves += self.orthogonal_left(row, col, board)
+        moves += self.orthogonal_down(row, col, board)
+        moves += self.orthogonal_right(row, col, board)
+        return moves
 
 
 class Knight(Piece):
-    def __init__(self, team: str, location: tuple) -> None:
+    def __init__(self, team: str, location: tuple) -> list:
         super().__init__(team, KNIGHT, location)
+
+    def knight_movement(self, start: tuple, offset: tuple, board: list) -> object:
+        moves = []
+        end = (start[0] + offset[0], start[1] + offset[1])
+        row_is_in_bounds = end[0] < len(board) and end[0] >= 0
+        col_is_in_bounds = end[1] < len(board) and end[1] >= 0
+        if row_is_in_bounds and col_is_in_bounds:
+            square_is_occupied = board[end[0]][end[1]]
+            if square_is_occupied:
+                is_enemy = self.detect_enemy_piece(board[end[0]][end[1]])
+                if is_enemy:
+                    moves.append(Movement(start, end, board))
+                return moves
+            moves.append(Movement(start, end, board))
+        return moves
+
+    def get_moves(self, row: int, col: int, board: list) -> list:
+        possible_moves = []
+        possible_moves += self.knight_movement((row, col), (-2, -1), board)
+        possible_moves += self.knight_movement((row, col), (-1, -2), board)
+        possible_moves += self.knight_movement((row, col), (1, -2), board)
+        possible_moves += self.knight_movement((row, col), (2, -1), board)
+        possible_moves += self.knight_movement((row, col), (2, 1), board)
+        possible_moves += self.knight_movement((row, col), (1, 2), board)
+        possible_moves += self.knight_movement((row, col), (-1, 2), board)
+        possible_moves += self.knight_movement((row, col), (-2, 1), board)
+        return possible_moves
 
 
 class Bishop(Piece):
