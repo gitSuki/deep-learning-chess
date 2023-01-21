3d7e7b0fa30ba95f1f2a5b111e8c9e036d5c39e4
unknown <collinbrown226@gmail.com>
2023-01-21 13:17:00 -0600

    Make king movement method compatible with new chess piece classes

--- chess_piece.py
+++ chess_piece.py
@@ -144,59 +144,49 @@ class Pawn(Piece):
         super().__init__(team, PAWN, location)
 
     def get_moves(self, row: int, col: int, board: list) -> list:
-        possible_moves = []
+        moves = []
         if self.team == WHITE:
             # move forward
             if not board[row - 1][col]:
-                possible_moves.append(Movement((row, col), (row - 1, col), board))
+                moves.append(Movement((row, col), (row - 1, col), board))
                 # pawns can move two squares directly forward on their first move
                 # pawns can never move backward so they will always be at their starting row
                 # if they haven't moved
                 if row == 6 and not board[row - 2][col]:
-                    possible_moves.append(Movement((row, col), (row - 2, col), board))
+                    moves.append(Movement((row, col), (row - 2, col), board))
             # capture to the left
             if col - 1 >= 0:
                 piece_exists = board[row - 1][col - 1]
                 if piece_exists and board[row - 1][col - 1].team == BLACK:
-                    possible_moves.append(
-                        Movement((row, col), (row - 1, col - 1), board)
-                    )
+                    moves.append(Movement((row, col), (row - 1, col - 1), board))
             # capture to the right
             if col + 1 < len(board[row]):
                 piece_exists = board[row - 1][col + 1]
                 if piece_exists and board[row - 1][col + 1].team == BLACK:
-                    possible_moves.append(
-                        Movement((row, col), (row - 1, col + 1), board)
-                    )
+                    moves.append(Movement((row, col), (row - 1, col + 1), board))
 
         if self.team == BLACK:
             if (row + 1) < len(board):
                 # move forward
                 if not board[row + 1][col]:
-                    possible_moves.append(Movement((row, col), (row + 1, col), board))
+                    moves.append(Movement((row, col), (row + 1, col), board))
                     # pawns can move two squares directly forward on their first move
                     # pawns can never move backward so they will always be at their starting row
                     # if they haven't moved
                     if row == 1 and not board[row + 2][col]:
-                        possible_moves.append(
-                            Movement((row, col), (row + 2, col), board)
-                        )
+                        moves.append(Movement((row, col), (row + 2, col), board))
                 # capture to the left
                 if col + 1 < len(board[row]):
                     piece_exists = board[row + 1][col + 1]
                     if piece_exists and board[row + 1][col + 1].team == WHITE:
-                        possible_moves.append(
-                            Movement((row, col), (row + 1, col + 1), board)
-                        )
+                        moves.append(Movement((row, col), (row + 1, col + 1), board))
                 # capture to the right
                 if col - 1 >= 0:
                     piece_exists = board[row + 1][col - 1]
                     if piece_exists and board[row + 1][col - 1].team == WHITE:
-                        possible_moves.append(
-                            Movement((row, col), (row + 1, col - 1), board)
-                        )
+                        moves.append(Movement((row, col), (row + 1, col - 1), board))
 
-        return possible_moves
+        return moves
 
 
 class Rook(Piece):
@@ -223,25 +213,26 @@ class Knight(Piece):
         col_is_in_bounds = end[1] < len(board) and end[1] >= 0
         if row_is_in_bounds and col_is_in_bounds:
             square_is_occupied = board[end[0]][end[1]]
-            if square_is_occupied:
-                is_enemy = self.detect_enemy_piece(board[end[0]][end[1]])
-                if is_enemy:
-                    moves.append(Movement(start, end, board))
+            if not square_is_occupied:
+                moves.append(Movement(start, end, board))
                 return moves
-            moves.append(Movement(start, end, board))
+
+            is_enemy = self.detect_enemy_piece(board[end[0]][end[1]])
+            if is_enemy:
+                moves.append(Movement(start, end, board))
         return moves
 
     def get_moves(self, row: int, col: int, board: list) -> list:
-        possible_moves = []
-        possible_moves += self.knight_movement((row, col), (-2, -1), board)
-        possible_moves += self.knight_movement((row, col), (-1, -2), board)
-        possible_moves += self.knight_movement((row, col), (1, -2), board)
-        possible_moves += self.knight_movement((row, col), (2, -1), board)
-        possible_moves += self.knight_movement((row, col), (2, 1), board)
-        possible_moves += self.knight_movement((row, col), (1, 2), board)
-        possible_moves += self.knight_movement((row, col), (-1, 2), board)
-        possible_moves += self.knight_movement((row, col), (-2, 1), board)
-        return possible_moves
+        moves = []
+        moves += self.knight_movement((row, col), (-2, -1), board)
+        moves += self.knight_movement((row, col), (-1, -2), board)
+        moves += self.knight_movement((row, col), (1, -2), board)
+        moves += self.knight_movement((row, col), (2, -1), board)
+        moves += self.knight_movement((row, col), (2, 1), board)
+        moves += self.knight_movement((row, col), (1, 2), board)
+        moves += self.knight_movement((row, col), (-1, 2), board)
+        moves += self.knight_movement((row, col), (-2, 1), board)
+        return moves
 
 
 class Bishop(Piece):
@@ -249,12 +240,12 @@ class Bishop(Piece):
         super().__init__(team, BISHOP, location)
 
     def get_moves(self, row: int, col: int, board: list) -> list:
-        possible_moves = []
-        possible_moves += self.diagonal_up_left(row, col, board)
-        possible_moves += self.diagonal_down_left(row, col, board)
-        possible_moves += self.diagonal_down_right(row, col, board)
-        possible_moves += self.diagonal_up_right(row, col, board)
-        return possible_moves
+        moves = []
+        moves += self.diagonal_up_left(row, col, board)
+        moves += self.diagonal_down_left(row, col, board)
+        moves += self.diagonal_down_right(row, col, board)
+        moves += self.diagonal_up_right(row, col, board)
+        return moves
 
 
 class Queen(Piece):
@@ -262,18 +253,94 @@ class Queen(Piece):
         super().__init__(team, QUEEN, location)
 
     def get_moves(self, row: int, col: int, board: list) -> list:
-        possible_moves = []
-        possible_moves += self.orthogonal_up(row, col, board)
-        possible_moves += self.orthogonal_left(row, col, board)
-        possible_moves += self.orthogonal_down(row, col, board)
-        possible_moves += self.orthogonal_right(row, col, board)
-        possible_moves += self.diagonal_up_left(row, col, board)
-        possible_moves += self.diagonal_down_left(row, col, board)
-        possible_moves += self.diagonal_down_right(row, col, board)
-        possible_moves += self.diagonal_up_right(row, col, board)
-        return possible_moves
+        moves = []
+        moves += self.orthogonal_up(row, col, board)
+        moves += self.orthogonal_left(row, col, board)
+        moves += self.orthogonal_down(row, col, board)
+        moves += self.orthogonal_right(row, col, board)
+        moves += self.diagonal_up_left(row, col, board)
+        moves += self.diagonal_down_left(row, col, board)
+        moves += self.diagonal_down_right(row, col, board)
+        moves += self.diagonal_up_right(row, col, board)
+        return moves
 
 
 class King(Piece):
     def __init__(self, team: str, location: tuple) -> None:
         super().__init__(team, KING, location)
+
+    def get_moves(self, row: int, col: int, board: list):
+        moves = []
+
+        # move up
+        if (row - 1) >= 0:
+            if (
+                not board[row - 1][col]
+                or (board[row - 1][col].team == WHITE and self.team == BLACK)
+                or (board[row - 1][col].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row - 1, col), board))
+
+        # move up-left
+        if (row - 1) >= 0 and (col - 1) >= 0:
+            if (
+                not board[row - 1][col - 1]
+                or (board[row - 1][col - 1].team == WHITE and self.team == BLACK)
+                or (board[row - 1][col - 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row - 1, col - 1), board))
+
+        # move left
+        if (col - 1) >= 0:
+            if (
+                not board[row][col - 1]
+                or (board[row][col - 1].team == WHITE and self.team == BLACK)
+                or (board[row][col - 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row, col - 1), board))
+
+        # move down-left
+        if (row + 1) < len(board) and (col - 1) >= 0:
+            if (
+                not board[row + 1][col - 1]
+                or (board[row + 1][col - 1].team == WHITE and self.team == BLACK)
+                or (board[row + 1][col - 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row + 1, col - 1), board))
+
+        # move down
+        if (row + 1) < len(board):
+            if (
+                not board[row + 1][col]
+                or (board[row + 1][col].team == WHITE and self.team == BLACK)
+                or (board[row + 1][col].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row + 1, col), board))
+
+        # move down-right
+        if (row + 1) < len(board) and (col + 1) < len(board):
+            if (
+                not board[row + 1][col + 1]
+                or (board[row + 1][col + 1].team == WHITE and self.team == BLACK)
+                or (board[row + 1][col + 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row + 1, col + 1), board))
+
+        # move right
+        if (col + 1) < len(board):
+            if (
+                not board[row][col + 1]
+                or (board[row][col + 1].team == WHITE and self.team == BLACK)
+                or (board[row][col + 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row, col + 1), board))
+
+        # move up-right
+        if (row - 1) >= 0 and (col + 1) < len(board):
+            if (
+                not board[row - 1][col + 1]
+                or (board[row - 1][col + 1].team == WHITE and self.team == BLACK)
+                or (board[row - 1][col + 1].team == BLACK and self.team == WHITE)
+            ):
+                moves.append(Movement((row, col), (row - 1, col + 1), board))
+        return moves
