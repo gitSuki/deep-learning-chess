5c867067844794d872cb266860ecbcc5817930cf
unknown <collinbrown226@gmail.com>
2023-01-21 05:08:24 -0600

    Create enums for defining the chess piece types and teams

---
+++ chess_piece.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class Team(Enum):
+    """
+    Defines both teams involved in the game.
+    """
+
+    WHITE = "white"
+    BLACK = "black"
+
+
+class Type(Enum):
+    """
+    Defines all possible types of chess pieces
+    """
+
+    PAWN = "pawn"
+    ROOK = "rook"
+    KNIGHT = "knight"
+    BISHOP = "bishop"
+    QUEEN = "queen"
+    KING = "king"
