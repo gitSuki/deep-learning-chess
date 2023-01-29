1bce9d9662c77f718791c2bb2937cd21fe7dfb8b
gitSuki <collinbrown226@gmail.com>
2023-01-28 19:56:23 -0600

    Add score board method for calculating the AI score of the current game state

--- ai.py
+++ ai.py
@@ -1,7 +1,30 @@
 import random
 
+from constants import *
+
+CHECKMATE = 1000
+STALEMATE = 0
+
+
 def find_random_move(legal_moves):
     # randint is inclusive of last value
     if len(legal_moves) >= 1:
         random_index = random.randint(0, len(legal_moves) - 1)
         return legal_moves[random_index]
+
+
+def find_best_move(game_state, legal_moves):
+    pass
+
+
+def score_board(board: list) -> int:
+    score = 0
+    for row in board:
+        for col in board:
+            square = board[row][col]
+            if square.team == WHITE:
+                score += square.ai_value
+            elif square.team == BLACK:
+                score -= square.ai_value
+
+    return score
