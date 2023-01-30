a6cabd4afb87923cee1b3ac6bacdf5a88a0e8ea6
gitSuki <collinbrown226@gmail.com>
2023-01-30 12:36:23 -0600

    Create helper function for calculating king movement

--- pieces/king.py
+++ pieces/king.py
@@ -1,6 +1,7 @@
 from constants import *
 from movement import Movement
-from pieces.main import Piece
+from pieces.main import Piece, detect_enemy_piece
+
 
 class King(Piece):
     def __init__(self, team: str, location: tuple) -> None:
@@ -9,76 +10,29 @@ class King(Piece):
 
     def get_moves(self, row: int, col: int, board: list):
         moves = []
-
-        # move up
-        if (row - 1) >= 0:
-            if (
-                not board[row - 1][col]
-                or (board[row - 1][col].team == WHITE and self.team == BLACK)
-                or (board[row - 1][col].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row - 1, col), board))
-
-        # move up-left
-        if (row - 1) >= 0 and (col - 1) >= 0:
-            if (
-                not board[row - 1][col - 1]
-                or (board[row - 1][col - 1].team == WHITE and self.team == BLACK)
-                or (board[row - 1][col - 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row - 1, col - 1), board))
-
-        # move left
-        if (col - 1) >= 0:
-            if (
-                not board[row][col - 1]
-                or (board[row][col - 1].team == WHITE and self.team == BLACK)
-                or (board[row][col - 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row, col - 1), board))
-
-        # move down-left
-        if (row + 1) < len(board) and (col - 1) >= 0:
-            if (
-                not board[row + 1][col - 1]
-                or (board[row + 1][col - 1].team == WHITE and self.team == BLACK)
-                or (board[row + 1][col - 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row + 1, col - 1), board))
-
-        # move down
-        if (row + 1) < len(board):
-            if (
-                not board[row + 1][col]
-                or (board[row + 1][col].team == WHITE and self.team == BLACK)
-                or (board[row + 1][col].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row + 1, col), board))
-
-        # move down-right
-        if (row + 1) < len(board) and (col + 1) < len(board):
-            if (
-                not board[row + 1][col + 1]
-                or (board[row + 1][col + 1].team == WHITE and self.team == BLACK)
-                or (board[row + 1][col + 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row + 1, col + 1), board))
-
-        # move right
-        if (col + 1) < len(board):
-            if (
-                not board[row][col + 1]
-                or (board[row][col + 1].team == WHITE and self.team == BLACK)
-                or (board[row][col + 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row, col + 1), board))
-
-        # move up-right
-        if (row - 1) >= 0 and (col + 1) < len(board):
-            if (
-                not board[row - 1][col + 1]
-                or (board[row - 1][col + 1].team == WHITE and self.team == BLACK)
-                or (board[row - 1][col + 1].team == BLACK and self.team == WHITE)
-            ):
-                moves.append(Movement((row, col), (row - 1, col + 1), board))
+        moves += king_movement((row, col), (-1, 0), board, self.team)
+        moves += king_movement((row, col), (-1, -1), board, self.team)
+        moves += king_movement((row, col), (0, -1), board, self.team)
+        moves += king_movement((row, col), (1, -1), board, self.team)
+        moves += king_movement((row, col), (1, 0), board, self.team)
+        moves += king_movement((row, col), (1, 1), board, self.team)
+        moves += king_movement((row, col), (0, 1), board, self.team)
+        moves += king_movement((row, col), (-1, 1), board, self.team)
         return moves
+
+
+def king_movement(start: tuple, offset: tuple, board: list, team: str) -> object:
+    moves = []
+    end = (start[0] + offset[0], start[1] + offset[1])
+    row_is_in_bounds = end[0] < len(board) and end[0] >= 0
+    col_is_in_bounds = end[1] < len(board) and end[1] >= 0
+    if row_is_in_bounds and col_is_in_bounds:
+        square_is_occupied = board[end[0]][end[1]]
+        if not square_is_occupied:
+            moves.append(Movement(start, end, board))
+            return moves
+
+        is_enemy = detect_enemy_piece(board[end[0]][end[1]], team)
+        if is_enemy:
+            moves.append(Movement(start, end, board))
+    return moves
