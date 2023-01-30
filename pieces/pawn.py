266f4f70da41a2fa7665c7931fb089c994e00147
gitSuki <collinbrown226@gmail.com>
2023-01-30 13:12:40 -0600

    Add helper functions for calculating if pawns can capture to their left or right

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -1,6 +1,7 @@
 from constants import *
 from movement import Movement
-from pieces.main import Piece
+from pieces.main import Piece, detect_enemy_piece
+
 
 class Pawn(Piece):
     def __init__(self, team: str, location: tuple) -> None:
@@ -18,16 +19,8 @@ class Pawn(Piece):
                 # if they haven't moved
                 if row == 6 and not board[row - 2][col]:
                     moves.append(Movement((row, col), (row - 2, col), board))
-            # capture to the left
-            if col - 1 >= 0:
-                piece_exists = board[row - 1][col - 1]
-                if piece_exists and board[row - 1][col - 1].team == BLACK:
-                    moves.append(Movement((row, col), (row - 1, col - 1), board))
-            # capture to the right
-            if col + 1 < len(board[row]):
-                piece_exists = board[row - 1][col + 1]
-                if piece_exists and board[row - 1][col + 1].team == BLACK:
-                    moves.append(Movement((row, col), (row - 1, col + 1), board))
+                moves += capture_left_movement((row, col), board, self.team)
+                moves += capture_right_movement((row, col), board, self.team)
 
         if self.team == BLACK:
             if (row + 1) < len(board):
@@ -39,15 +32,56 @@ class Pawn(Piece):
                     # if they haven't moved
                     if row == 1 and not board[row + 2][col]:
                         moves.append(Movement((row, col), (row + 2, col), board))
-                # capture to the left
-                if col + 1 < len(board[row]):
-                    piece_exists = board[row + 1][col + 1]
-                    if piece_exists and board[row + 1][col + 1].team == WHITE:
-                        moves.append(Movement((row, col), (row + 1, col + 1), board))
-                # capture to the right
-                if col - 1 >= 0:
-                    piece_exists = board[row + 1][col - 1]
-                    if piece_exists and board[row + 1][col - 1].team == WHITE:
-                        moves.append(Movement((row, col), (row + 1, col - 1), board))
-
+                moves += capture_left_movement((row, col), board, self.team)
+                moves += capture_right_movement((row, col), board, self.team)
         return moves
+
+
+def pawn_movement(start: tuple, offset: tuple, board: list, team: str) -> list:
+    """
+    Generates a list of movement objects for a Knight based on a given offset. Returns an empty list if the offset is not a valid movement.
+    """
+    moves = []
+    end = (start[0] + offset[0], start[1] + offset[1])
+    row_is_in_bounds = end[0] < len(board) and end[0] >= 0
+    col_is_in_bounds = end[1] < len(board) and end[1] >= 0
+    if row_is_in_bounds and col_is_in_bounds:
+        square_is_occupied = board[end[0]][end[1]]
+        if not square_is_occupied:
+            moves.append(Movement(start, end, board))
+            return moves
+
+        is_enemy = detect_enemy_piece(board[end[0]][end[1]], team)
+        if is_enemy:
+            moves.append(Movement(start, end, board))
+    return moves
+
+
+def capture_left_movement(start: tuple, board: list, team: str) -> list:
+    moves = []
+    row = start[0] - 1 if team is WHITE else start[0] + 1
+    col = start[1] - 1
+
+    in_bounds = col >= 0 and col < len(board)
+    if in_bounds:
+        square_is_occupied = board[row][col]
+        if square_is_occupied:
+            is_enemy = detect_enemy_piece(board[row][col], team)
+            if is_enemy:
+                moves.append(Movement(start, (row, col), board))
+    return moves
+
+
+def capture_right_movement(start: tuple, board: list, team: str) -> list:
+    moves = []
+    row = start[0] - 1 if team is WHITE else start[0] + 1
+    col = start[1] + 1
+
+    in_bounds = col >= 0 and col < len(board)
+    if in_bounds:
+        square_is_occupied = board[row][col]
+        if square_is_occupied:
+            is_enemy = detect_enemy_piece(board[row][col], team)
+            if is_enemy:
+                moves.append(Movement(start, (row, col), board))
+    return moves
